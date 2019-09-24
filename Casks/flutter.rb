0f62477d905ac6bb6bee4f3e4ffae2fb86e9b104
Max Gabrielsson <max.gabrielsson@squeed.com>
2019-09-24 13:24:12 +0200

    Fix shasum

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,6 +1,6 @@
 cask "flutter" do
   version :latest
-  sha256 "cec91922f563e3a0ff09a5420f8a6eda2a74ce45914a2b008cdf4da97e52c4bd"
+  sha256 "8f7a5cfa1494b6758e1b9b7913871a60934c7f3d487979a6ef176ce4f2a7b4a3"
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.9.1+hotfix.2-stable.zip"
 
   name "flutter"
