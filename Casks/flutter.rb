6d4a54a2bc7a452a5118435e101a6ba98e22d7c2
Frederik Schweiger <mail@flschweiger.net>
2019-11-17 13:00:30 +0100

    Upgrade to hotfix.6

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,7 +1,7 @@
 cask "flutter" do
   version :latest
-  sha256 "8f7a5cfa1494b6758e1b9b7913871a60934c7f3d487979a6ef176ce4f2a7b4a3"
-  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.9.1+hotfix.2-stable.zip"
+  sha256 "8d0b3e217e45fbde64e117c5932ec5bd18ced0e8e8fba80a0fec95e38854bb6a"
+  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.9.1+hotfix.6-stable.zip"
 
   name "flutter"
   homepage "https://flutter.dev"
